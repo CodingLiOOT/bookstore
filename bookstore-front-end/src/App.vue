47ef768a6d2b765ac7c991214433bbf0d4eb8730
nancy_wdi <nancy_wdi@163.com>
2021-07-08 11:08:29 +0800

    修改app

--- bookstore-front-end/src/App.vue
+++ bookstore-front-end/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <el-container>
+      <el-header class="el-header">
+        <el-row>
+          <el-col :span="6">
+            <el-link type="primary">登录/注册</el-link>
+          </el-col>
+        </el-row>
+      </el-header>
+    </el-container>
     <router-view/>
   </div>
 </template>
@@ -17,6 +26,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  /*margin-top: 60px;*/
+  width: 100%;
+  background-size: 100%;
+  min-height: 1000px;
+  background: rgba(38, 50, 56, 0.6) url(./assets/login_bg.jpg) no-repeat fixed top center;
 }
+html,body{
+  margin:0;
+  padding:0;
+}
+.el-header{
+  background-color: #e5e8f5;
+  color: #333;
+  text-align: center;
+  line-height: 50px;
+}
+
 </style>
---
+++ bookstore-front-end/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-header class="el-header">
+    <el-row>
+      <el-col :span="6">
+        <el-link type="primary">登录/注册</el-link>
+      </el-col>
+    </el-row>
+  </el-header>
+</template>
+
+<script>
+export default {
+name: "Header"
+}
+</script>
+
+<style scoped>
+.el-header{
+  background-color: #e5e8f5;
+  color: #333;
+  text-align: center;
+  line-height: 50px;
+}
+</style>
