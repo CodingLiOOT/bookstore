4bfc729e378d3f77e90bd83094095f860dfd617f
Wang20001121 <18301051@bjtu.edu.cn>
2021-07-09 19:12:54 +0800

    修改的后端购物车

--- bookstore-back-end/src/main/java/com/bjtu/bookstore/mapper/BookMapper.java
+++ bookstore-back-end/src/main/java/com/bjtu/bookstore/mapper/BookMapper.java
@@ -1,10 +1,14 @@
 package com.bjtu.bookstore.mapper;
 
 import com.bjtu.bookstore.entity.Store;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
+@Mapper
+@Repository
 public interface BookMapper {
     //    找到该书的价钱
     @Select("select price from book where id =  #{bookid}")
