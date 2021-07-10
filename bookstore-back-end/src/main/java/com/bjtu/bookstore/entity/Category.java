689e29b3a9e94fb28d4dcff569257277f028eeac
CodeingLiOOT <296277916@qq.com>
2021-07-10 14:41:24 +0800

    feat: 类别后端接口

--- bookstore-back-end/src/main/java/com/bjtu/bookstore/entity/Category.java
+++ bookstore-back-end/src/main/java/com/bjtu/bookstore/entity/Category.java
@@ -2,6 +2,9 @@ package com.bjtu.bookstore.entity;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @program: bookstore-back-end
  * @description: category entity
@@ -12,11 +15,20 @@ import lombok.Data;
 @Data
 public class Category {
 
-    private String id;
+    private Integer id;
 
     private String name;
 
     private String desc;
 
-    private String parentId;
+    private Integer parentId;
+
+    private List<Category> childCategory;
+
+    public void addItem(Category c) {
+        if (this.childCategory == null) {
+            this.childCategory = new ArrayList<>();
+        }
+        this.childCategory.add(c);
+    }
 }
---
+++ bookstore-back-end/src/main/java/com/bjtu/bookstore/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.bjtu.bookstore.mapper;
+
+import com.bjtu.bookstore.entity.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface CategoryMapper {
+
+    @Select("select * from category")
+    List<Category> getCategories();
+}
